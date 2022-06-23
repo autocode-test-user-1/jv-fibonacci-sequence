785f8c14032bdc0151afc5bca8c8fd5b6db683e4
autocode-test-user-1 <70530690+autocode-test-user-1@users.noreply.github.com>
2022-06-23 19:12:14 +0600

    Update FibonacciSequenceTest.java

--- src/test/java/core/basesyntax/FibonacciSequenceTest.java
+++ src/test/java/core/basesyntax/FibonacciSequenceTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class FibonacciSequenceTest {
-    private static final int[] FIBONACCI_SEQUENCE = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+    private static final int[] FIBONACCI_SEQUENCE = {0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
             233, 377, 610, 987, 1597, 2584, 4181};
 
     @Test
